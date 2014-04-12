8ca9f8b7c3233436c09a5e90edd134b09eb3f64f
= <thomas07@vt.edu>
2014-04-11 22:50:09 -0700

    First commit. README.md update, db create, rails_12factor inclusion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = '02188fb4589596c5cd74708a3fde677fa2cbb9e66510973879f2de1076a4bd3cb9db8df557cc2d71d5a6e6a33b39d085e4468c929572cfb5b96cf179215d2d92'
