bf29fca5b6acef393920c94482fd7beac213aa8b
= <thomas07@vt.edu>
2014-04-17 15:00:57 -0700

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,37 @@
-
 require 'faker'
 
-24.times do 
-  p = Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
-  3.times do
-    p.comments.create(body: Faker::Lorem.paragraph)
+# Create 5 users with their own posts
+5.times do
+  password = Faker::Lorem.characters(10)
+  user = User.new(
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    password: password, 
+    password_confirmation: password)
+  user.skip_confirmation!
+  user.save
+
+  # Note: by calling `User.new` instead of `create`,
+  # we create an instance of a user which isn't saved to the database.
+  # The `skip_confirmation!` method sets the confirmation date
+  # to avoid sending an email. The `save` method updates the database.
+
+  rand(5..10).times do
+    post = Post.create(
+      user: user,
+      title: Faker::Lorem.sentence, 
+      body: Faker::Lorem.paragraph)
+    # set the created_at to a time within the past year
+    post.update_attribute(:created_at, Time.now - rand(600..31536000))
   end
 end
 
-puts "Seed Finished"
-puts "#{Post.count} posts created."
-puts "#{Comment.count} comments created."
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes(email: 'thomas07vt@gmail.com', password: 'password', password_confirmation: 'password')
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+
