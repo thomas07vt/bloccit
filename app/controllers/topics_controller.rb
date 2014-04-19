d80d4f13161b3e677d640be0d23321147c8ee898
= <thomas07@vt.edu>
2014-04-19 00:29:01 -0700

    Cleaned up registration views and app layout

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -20,7 +20,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    @topic = Topic.new(topic_params)
     authorize(@topic)
     if @topic.save
       flash[:notice] = "Topic was created."
@@ -34,7 +34,7 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize(@topic)
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+    if @topic.update_attributes(topic_params)
       flash[:notice] = "Topic was updated."
       redirect_to @topic
     else
@@ -42,4 +42,11 @@ class TopicsController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
+  
 end
