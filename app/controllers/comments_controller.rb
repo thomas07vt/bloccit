d41d104f111686dddbf990c210c438eb83510cde
= <thomas07@vt.edu>
2014-04-23 14:32:25 -0700

    Added the 'workaround' to comment errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,9 +12,13 @@ class CommentsController < ApplicationController
 
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@post.topic, @post]
+      redirect_to [@post.topic, @post] 
+      #"Reset" this session variable. Otherwise, old messages appear in the posts.show view
+      session[:comment_errors] = nil
     else
-      flash[:error] = "There was an error saving the comment. Please try again. Minimum character length: 5"
+      flash[:error] = "There was an error saving the comment. Please try again."
+      #store error message in session
+      session[:comment_errors] = @comment.errors[:body]
       redirect_to [@post.topic, @post]
     end
   end
