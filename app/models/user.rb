400ca53f598584d1c888f1c8f4c58385e6e510a0
= <thomas07@vt.edu>
2014-04-24 12:51:36 -0700

    Iterlude changes for registration, email permissions and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,9 @@ class User < ActiveRecord::Base
   def favorited(post)
     self.favorites.where(post_id: post.id).first
   end
+
+  def voted(post)
+    self.votes.where(post_id: post.id).first
+  end
   
 end
